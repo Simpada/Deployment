320cb7719dbf098a4352f3809f1c0594db652e5c
Marcus <60523971+Simpada@users.noreply.github.com>
2022-10-16 17:13:29 +0200

    Allow azure to select the port

--- src/main/java/no/kristiania/deployment/Program.java
+++ src/main/java/no/kristiania/deployment/Program.java
@@ -1,9 +1,14 @@
 package no.kristiania.deployment;
 
+import java.util.Optional;
+
 public class Program {
     public static void main(String[] args) throws Exception {
 
-        var webShop = new WebShopServer(8080);
-        webShop.start();
+        var port = Optional.ofNullable(System.getenv("HTTP_PLATFORM_PORT"))
+                .map(Integer::parseInt)
+                .orElse(8080);
+
+        new WebShopServer(port).start();
     }
 }
